007a74e07d6f30e203b6f6c4c93788ef6bf4ec1d
hemartino <justinebergavino5@gmail.com>
2025-11-17 13:03:59 +0800

    Flutter 3.32.5 V Firebase Integration Final Project

--- ecommerce_app/android/app/build.gradle.kts
+++ ecommerce_app/android/app/build.gradle.kts
@@ -31,6 +31,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        minSdk = 23
+
     }
 
     buildTypes {
